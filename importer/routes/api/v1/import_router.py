4a8db993505deb470b18c43d82343c23aeada7bb
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-10-04 13:46:30 +0200

    [feature] rasdaman importer

--- importer/routes/api/v1/import_router.py
+++ importer/routes/api/v1/import_router.py
@@ -16,23 +16,22 @@ import_endpoints = Blueprint('import_endpoints', __name__)
 def upload():
     """Uploads rasters to Rasdaman"""
     logging.info('[ROUTER] Importing rasters')
-    logging.info("Request json data:")
-    logging.info(request.json)
-    req_input = request.json['input']
-    try:
-        coverages_xml = RasdamanService.get_rasdaman_coverages()
-        coverages_list = XMLService.get_coverages(coverages_xml)
-        logging.info('coverages_list:')
-        logging.info(coverages_list)
-
-        if req_input['coverage_id'] not in coverages_list:
-            recipe = RecipeHelper.generate_recipe(req_input['paths'], req_input['coverage_id'])
-            logging.info('recipe:')
-            logging.info(recipe)
-
-    except XMLParserError:
-        return "NOT OK", 500
-    return "OK", 200
+    logging.info(request.get_json())
+    # req_input = request.json['input']
+    # try:
+    #     coverages_xml = RasdamanService.get_rasdaman_coverages()
+    #     coverages_list = XMLService.get_coverages(coverages_xml)
+    #     logging.info('coverages_list:')
+    #     logging.info(coverages_list)
+    #
+    #     if req_input['coverage_id'] not in coverages_list:
+    #         recipe = RecipeHelper.generate_recipe(req_input['paths'], req_input['coverage_id'])
+    #         logging.info('recipe:')
+    #         logging.info(recipe)
+    #
+    # except XMLParserError:
+    #     return "NOT OK", 500
+    return jsonify(request.get_json()), 200
 
 
 @import_endpoints.route('/import', strict_slashes=False, methods=['GET'])
